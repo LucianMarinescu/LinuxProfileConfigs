a5626284be530c86f2d48bbc9ee5032fddc8cf45
Lucian Marinescu <marinescu.gl@gmail.com>
2019-05-12 12:56:44 +0300

    Install yakuake; execute autoremove.

--- install-stuff.sh
+++ install-stuff.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install -y python3 python3-pip vim vim-gtk3 tmux git meld xfonts-terminus tree oracle-java8-installer minicom 
+sudo apt install -y python3 python3-pip vim vim-gtk3 tmux git meld xfonts-terminus tree oracle-java8-installer minicom yakuake
+sudo apt autoremove
 
