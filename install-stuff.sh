5a9467b7e81df0a737d7ed19fa9a4ee75bd2a007
Lucian Marinescu <marinescu.gl@gmail.com>
2020-06-06 03:36:53 +0300

    Install extra development-related packages

--- install-stuff.sh
+++ install-stuff.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install -y python3 python3-pip vim vim-gtk3 tmux git meld xfonts-terminus tree oracle-java8-installer minicom yakuake
+sudo apt install -y python3 python3-pip vim vim-gtk3 tmux git meld xfonts-terminus tree oracle-java8-installer minicom yakuake cmake build-essential apt-file make g++ gcc gdb screen
+sudo apt-file update
 sudo apt autoremove
 
